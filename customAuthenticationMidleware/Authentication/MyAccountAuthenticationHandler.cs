e63d6dd769ade460f7415f899ed3d6484077e02e
carlos <carlos.angel.campos@gmail.com>
2020-10-09 12:31:47 +0200

    custom Midleware

---
+++ customAuthenticationMidleware/Authentication/MyAccountAuthenticationHandler.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.IO;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace dotnetcore3app.AuthenticationHandlers
+{
+    // https://referbruv.com/blog/posts/implementing-custom-authentication-scheme-and-handler-in-aspnet-core-3x
+    public class MyAccountAuthenticationSchemeOptions
+        : AuthenticationSchemeOptions
+    { }
+
+    public class MyAccountAuthenticationHandler
+        : AuthenticationHandler<MyAccountAuthenticationSchemeOptions>
+    {
+        public MyAccountAuthenticationHandler(
+            IOptionsMonitor<MyAccountAuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            TokenModel model = new TokenModel();
+
+            // validation comes in here
+            if (!Request.Headers.ContainsKey("X-Token"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Header Not Found."));
+            }
+
+            var token = Request.Headers["X-Token"].ToString();
+
+            if (token == "barralibre")
+            {
+                model.EmailAddress = "barralibre@gmail.com";
+                model.Name = "nombreBarraLibre";
+                model.UserId = 1234;
+            }
+
+            if (model != null)
+            {
+                // success case AuthenticationTicket generation
+                // happens from here
+
+                // create claims array from the model
+                var claims = new[] {
+                    new Claim(ClaimTypes.NameIdentifier, model.UserId.ToString()),
+                    new Claim(ClaimTypes.Email, model.EmailAddress),
+                    new Claim(ClaimTypes.Name, model.Name) };
+
+                // generate claimsIdentity on the name of the class
+                var claimsIdentity = new ClaimsIdentity(claims,
+                            nameof(MyAccountAuthenticationHandler));
+
+                // generate AuthenticationTicket from the Identity
+                // and current authentication scheme
+                // Construyen objetos AuthenticationTicket que representan la identidad del usuario 
+                // si la autenticación se realiza correctamente.
+                var ticket = new AuthenticationTicket(
+                    new ClaimsPrincipal(claimsIdentity), this.Scheme.Name);
+
+                // pass on the ticket to the middleware
+                return Task.FromResult(AuthenticateResult.Success(ticket));
+            }
+
+            return Task.FromResult(AuthenticateResult.Fail("Model is Empty"));
+        }
+    }
+
+    public class TokenModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ customAuthenticationMidleware/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetcore3app.AuthenticationHandlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.IO;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace contenedorNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+             services.AddAuthentication(options =>
+              {
+                  options.DefaultScheme = "MyAccountAuthenticationHandler";
+              })
+            .AddScheme<MyAccountAuthenticationSchemeOptions, MyAccountAuthenticationHandler>
+                    ("MyAccountAuthenticationHandler", op => { });
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
